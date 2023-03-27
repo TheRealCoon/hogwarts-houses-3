d1880b7a05098335800c776b21bdc64a4828ff2d
Jutsz <judit.fulmer@gmail.com>
2023-03-27 18:26:58 +0200

    add entity annotation to teacher

--- src/main/java/com/codecool/hogwartshouses/model/Teacher.java
+++ src/main/java/com/codecool/hogwartshouses/model/Teacher.java
@@ -1,4 +1,12 @@
 package com.codecool.hogwartshouses.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Teacher {
+    @Id
+    private Long id;
+
+
 }
