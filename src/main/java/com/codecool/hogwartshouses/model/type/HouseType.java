ac21aa106479ea2e24b93b148fe24195ee230951
br0k1z0l1 <105515733+br0k1z0l1@users.noreply.github.com>
2023-03-27 16:27:26 +0200

    Delete src directory

--- src/main/java/com/codecool/hogwartshouses/model/type/HouseType.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.hogwartshouses.model.type;
-
-public enum HouseType {
-    GRYFFINDOR,
-    HUFFLEPUFF,
-    RAVENCLAW,
-    SLYTHERIN
-}
--- src/main/java/com/codecool/hogwartshouses/model/type/PetType.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.hogwartshouses.model.type;
-
-public enum PetType {
-    CAT,
-    RAT,
-    OWL,
-    NONE
-}
