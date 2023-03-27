a760db4c3dde4231dc825cb4d5407ee9b32287c2
Jutsz <judit.fulmer@gmail.com>
2023-03-27 18:28:31 +0200

    implement WandRepository

---
+++ src/main/java/com/codecool/hogwartshouses/DAO/WandRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.hogwartshouses.DAO;
+
+import com.codecool.hogwartshouses.model.Wand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WandRepository extends JpaRepository<Wand, Long> {
+}
