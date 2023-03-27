67ddfebe2c3a52f4d74efbe956fbb241aa3582b4
T.Sanyi <s.truczkai@gmail.com>
2023-03-28 00:04:02 +0200

    implement Spell model class

--- src/main/java/com/codecool/hogwartshouses/model/Spell.java
+++ src/main/java/com/codecool/hogwartshouses/model/Spell.java
@@ -1,12 +1,25 @@
 package com.codecool.hogwartshouses.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Spell {
     @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    private String name;
+    private Integer power;
+    private boolean isBanned;
+    @ManyToMany
+    //TODO set up many-to-many
+    private Set<Wand> wands;
 }
