812004ca3bf186d0d0ef85a5cf00f8bba12d383f
T.Sanyi <s.truczkai@gmail.com>
2023-03-28 00:42:06 +0200

    add Repository annotation to WandRepository

--- src/main/java/com/codecool/hogwartshouses/DAO/WandRepository.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/WandRepository.java
@@ -2,6 +2,8 @@ package com.codecool.hogwartshouses.DAO;
 
 import com.codecool.hogwartshouses.model.Wand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WandRepository extends JpaRepository<Wand, Long> {
 }
