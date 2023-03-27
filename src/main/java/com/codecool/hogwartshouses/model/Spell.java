4574fcefcac96f20fe4d14f48ee5f1e7a15c35b5
Jutsz <judit.fulmer@gmail.com>
2023-03-27 18:26:43 +0200

    add entity annotation to spell

--- src/main/java/com/codecool/hogwartshouses/model/Spell.java
+++ src/main/java/com/codecool/hogwartshouses/model/Spell.java
@@ -1,4 +1,12 @@
 package com.codecool.hogwartshouses.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Spell {
+    @Id
+    private Long id;
+
+
 }
