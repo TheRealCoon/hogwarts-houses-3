fcbdccc22b95d8a45d9d1fa50541dc4d3f2468ee
T.Sanyi <s.truczkai@gmail.com>
2023-03-28 14:18:12 +0200

    add usage field to spells

--- src/main/java/com/codecool/hogwartshouses/model/Spell.java
+++ src/main/java/com/codecool/hogwartshouses/model/Spell.java
@@ -17,6 +17,7 @@ public class Spell {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String usage;
     private Integer power;
     private boolean isBanned;
     @ManyToMany
