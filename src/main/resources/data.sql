c2dcad0a07f35850cbd7491b6d3cd81aec6bf78f
Kalapis Dorottya <kalapisd@gmail.com>
2023-03-28 14:06:21 +0200

    modify data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -86,7 +86,20 @@ INSERT INTO ingredients(NAME, RECIPE_ID) values ('LEECHES', 5);
 
 INSERT INTO studentrecipes(STUDENT_ID, RECIPE_ID) values (1, 2);
 
+insert into wands(wood_type, color)
+values ('wand type1', 'black'),
+       ('wand type2', 'white');
 
+insert into teachers(name, subject, is_witch, age, wand_id)
+values ('teacher name1', 'subject1', true, 30, 1),
+       ('teacher name2', 'subejct2', false, 100, 2);
+
+insert into spells(name, usage, power, is_banned)
+values ('spell1', 'usage1', 10, true),
+       ('spell2', 'usage2', 50, false);
+
+insert into wand_spell(wand_id, spell_id)
+values (1,1), (1,2), (2,1);
 
 
 
