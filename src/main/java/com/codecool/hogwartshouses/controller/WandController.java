91e845874dce388d12c7ba75a6a5712715966b2d
Jutsz <judit.fulmer@gmail.com>
2023-03-29 15:59:22 +0200

    add @RequestMapping("/wands") to WandController

--- src/main/java/com/codecool/hogwartshouses/controller/WandController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/WandController.java
@@ -4,11 +4,13 @@ import com.codecool.hogwartshouses.model.WandDTO;
 import com.codecool.hogwartshouses.service.WandService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/wands")
 public class WandController {
     private WandService wandService;
 
@@ -16,12 +18,12 @@ public class WandController {
         this.wandService = wandService;
     }
 
-    @GetMapping("/wands")
+    @GetMapping
     public List<WandDTO> getWands(){
         return wandService.getWands();
     }
 
-    @GetMapping("/wands/{id}")
+    @GetMapping("/{id}")
     public WandDTO getWandById(@PathVariable Long id){
         return wandService.getWandById(id);
     }
