99bb575fa69d0fb8132ca77960ea0038dd8afc21
T.Sanyi <s.truczkai@gmail.com>
2023-04-03 09:23:44 +0200

    delete comments in TeacherRepository

--- src/main/java/com/codecool/hogwartshouses/DAO/TeacherRepository.java
+++ src/main/java/com/codecool/hogwartshouses/DAO/TeacherRepository.java
@@ -2,14 +2,10 @@ package com.codecool.hogwartshouses.DAO;
 
 import com.codecool.hogwartshouses.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
-    //@Query("SELECT t FROM Teacher t where t.wand.woodType = :woodType")
-    //List<Teacher> findTeacherByWand(@Param("woodType") String woodType);
     List<Teacher> findAllByWand_WoodType(String woodType);
 }
